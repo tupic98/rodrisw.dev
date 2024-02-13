f5b661b2d76022fb292753a094c4490620914d78
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-02-12 22:18:27 -0800

    Add sass support

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,8 @@ import "@css/flaticon.min.css";
 import "@css/fontawesome-5.14.0.min.css";
 import "@css/nice-select.min.css";
 import "@css/slick.min.css";
-import "@css/style.css";
+// import "@css/style.css";
+import "@sass/style.sass";
 import "./globals.css";
 
 /** google fonts */

b581b616fe8eeb52566fa0cb14b5950cd8be0dd1
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-02-22 00:05:06 -0800

    Modify headline marquee

---
+++ components/Headline.js
@@ -0,0 +1,128 @@
+"use client"
+
+const Headline = () => {
+    return (
+      <div className="headline-area-h2 rel z-2 py-25">
+        <div className="headline-wrap">
+          <span className="marquee-wrap">
+            <span className="marquee-inner left">
+              <span className="marquee-item">
+                Vue.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Nuxt.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SEO
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Wordpress
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Shopify
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                TailwindCSS
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                React
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Next.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SASS
+                <i className="far fa-asterisk" />
+              </span>
+            </span>
+            <span className="marquee-inner left">
+              <span className="marquee-item">
+                Vue.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Nuxt.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SEO
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Wordpress
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Shopify
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                TailwindCSS
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                React
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Next.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SASS
+                <i className="far fa-asterisk" />
+              </span>
+            </span>
+            <span className="marquee-inner left">
+              <span className="marquee-item">
+                Vue.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Nuxt.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SEO
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Wordpress
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Shopify
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                TailwindCSS
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                React
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                Next.js
+                <i className="far fa-asterisk" />
+              </span>
+              <span className="marquee-item">
+                SASS
+                <i className="far fa-asterisk" />
+              </span>
+            </span>
+          </span>
+        </div>
+      </div>
+    );
+}
+
+export default Headline;

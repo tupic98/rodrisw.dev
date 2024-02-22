4c1576559ba8bbc3f69f16574464a304801afe08
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2024-02-22 00:04:42 -0800

    Modify home layout

--- app/page.js
+++ app/page.js
@@ -1,52 +1,37 @@
 import About from "@/components/About";
-import Blog from "@/components/Blog";
-import Clients from "@/components/Clients";
 import Contact from "@/components/Contact";
-import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+import { Hero2 } from "@/components/Hero";
 import Projects from "@/components/Projects";
-import Resume from "@/components/Resume";
-import Services from "@/components/Services";
+import { Resume2 } from "@/components/Resume";
 import Skill from "@/components/Skill";
-import Testimonial from "@/components/Testimonial";
 import Layout from "@/layout/Layout";
+import ScrollTop from "@/components/ScrollTop";
+import Headline from "@/components/Headline";
 
 export default function Home() {
   return (
     <Layout footer={2}>
       {/* Hero Section Start */}
-      <Hero />
+      {/* <Hero /> */}
+      <Hero2 />
       {/* Hero Section End */}
       {/* About Area start */}
       <About />
       {/* About Area end */}
       {/* Resume Area start */}
-      <Resume />
+      <Resume2 />
+      <Headline />
       {/* Resume Area end */}
-      {/* Services Area start */}
-      <Services />
-      {/* Services Area end */}
       {/* Skill Area start */}
       <Skill />
       {/* Skill Area end */}
       {/* Projects Area start */}
       <Projects />
       {/* Projects Area end */}
-      {/* Testimonial Area start */}
-      <Testimonial />
-      {/* Testimonial Area end */}
-      {/* Pricing Area start */}
-      <Pricing />
-      {/* Pricing Area end */}
       {/* Contact Area start */}
       <Contact />
       {/* Contact Area end */}
-      {/* Blog Area start */}
-      <Blog />
-      {/* Blog Area end */}
-      {/* Client Log start */}
-      <Clients />
-      {/* Client Log end */}
+      <ScrollTop />
     </Layout>
   );
 }
